82ca85f4e7913289d23d2145437c6c3250cce0ab
Hamza Yawa <coachbabaa@gmail.com>
2023-07-23 20:45:49 +0100

    Add tasks

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,20 +6,20 @@ listening on 0.0.0.0, port 5000. 1.
 
 from flask import Flask
 
-
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_world():
-    """ Returns Hello HBNB. """
-    return 'Hello HBNB!'
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hello():
-    """ Return HBNB """
-    return 'HBNB'
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB'."""
+    return "HBNB"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
